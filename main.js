dd565b7e25a38870f6491e578a9188718badbc13
Atmaram07 <ag4atmaramgreat@gmail.com>
2020-07-26 12:48:57 +0530

    updated

--- main.js
+++ main.js
@@ -1,50 +1,127 @@
-let board = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-];
+// HTML Elements
+const statusDiv = document.querySelector('.status');
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelectorAll('.game-cell');
 
-let p1Counter = 1;
-let p2Counter = 1;
+// game constants
+const xSymbol = '×';
+const oSymbol = '○';
 
-class Player {
-    constructor() {
-      this.isTurn = false;
-      this.win = false;
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
+
+
+// functions
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+
+const handleWin = (letter) => {
+  gameIsLive = false;
+  if (letter === 'x') {
+    statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
+  } else {
+    statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
+  }
+};
+
+const checkGameStatus = () => {
+  const topLeft = cellDivs[0].classList[1];
+  const topMiddle = cellDivs[1].classList[1];
+  const topRight = cellDivs[2].classList[1];
+  const middleLeft = cellDivs[3].classList[1];
+  const middleMiddle = cellDivs[4].classList[1];
+  const middleRight = cellDivs[5].classList[1];
+  const bottomLeft = cellDivs[6].classList[1];
+  const bottomMiddle = cellDivs[7].classList[1];
+  const bottomRight = cellDivs[8].classList[1];
+
+  // check winner
+  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[1].classList.add('won');
+    cellDivs[2].classList.add('won');
+  } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+    handleWin(middleLeft);
+    cellDivs[3].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[5].classList.add('won');
+  } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+    handleWin(bottomLeft);
+    cellDivs[6].classList.add('won');
+    cellDivs[7].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[3].classList.add('won');
+    cellDivs[6].classList.add('won');
+  } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+    handleWin(topMiddle);
+    cellDivs[1].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[7].classList.add('won');
+  } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+    handleWin(topRight);
+    cellDivs[2].classList.add('won');
+    cellDivs[5].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+    handleWin(topRight);
+    cellDivs[2].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[6].classList.add('won');
+  } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
+    gameIsLive = false;
+    statusDiv.innerHTML = 'Game is tied!';
+  } else {
+    xIsNext = !xIsNext;
+    if (xIsNext) {
+      statusDiv.innerHTML = `${xSymbol} is next`;
+    } else {
+      statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
     }
   }
-class Space {
-  constructor(id) {
-    this.played = false;
-    this.id = id;
+};
+
+
+// event Handlers
+const handleReset = () => {
+  xIsNext = true;
+  statusDiv.innerHTML = `${xSymbol} is next`;
+  for (const cellDiv of cellDivs) {
+    cellDiv.classList.remove('x');
+    cellDiv.classList.remove('o');
+    cellDiv.classList.remove('won');
+  }
+  gameIsLive = true;
+};
+
+const handleCellClick = (e) => {
+  const classList = e.target.classList;
+
+  if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
+    return;
   }
 
-  addSymbolX(space) {
-    $(space).append("<div class='x'>X</div>");
+  if (xIsNext) {
+    classList.add('x');
+    checkGameStatus();
+  } else {
+    classList.add('o');
+    checkGameStatus();
   }
-  
-  addSymbolO(space) {
-    $(space).append("<div class='o'>O</div>");
-  }
+};
+
+
+// event listeners
+resetDiv.addEventListener('click', handleReset);
+
+for (const cellDiv of cellDivs) {
+  cellDiv.addEventListener('click', handleCellClick)
 }
-
-//create players
-const player1 = new Player();
-const player2 = new Player();
-
-//create spaces
-const space1 = new Space('one');
-const space2 = new Space('two');
-const space3 = new Space('three');
-const space4 = new Space('four');
-const space5 = new Space('five');
-const space6 = new Space('six');
-const space7 = new Space('seven');
-const space8 = new Space('eight');
-const space9 = new Space('nine');
-
-//switch turns
-function switchTurns(one, two) {
-    one.isTurn = true;
-    two.isTurn = false;
-  }
